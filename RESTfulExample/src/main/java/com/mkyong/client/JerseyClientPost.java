5a8aa4db8322c50e7264e0a6eb3b727c6f5f37cd
Michal Kostrzewa <misiek.mk@gmail.com>
2016-09-08 20:07:10 +0200

    REST POST BYTE ARRAY ! + git ignore ResfExample

--- RESTfulExample/src/main/java/com/mkyong/client/JerseyClientPost.java
+++ RESTfulExample/src/main/java/com/mkyong/client/JerseyClientPost.java
@@ -4,18 +4,28 @@ import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Base64;
+
 public class JerseyClientPost {
 
 	public static void main(String[] args) {
 
 		try {
+			Path path = Paths.get("C:\\kth.jpg");
+			byte[] data = Files.readAllBytes(path);
+			String base64String = Base64.getEncoder().encodeToString(data);
 
 			Client client = Client.create();
 
 			WebResource webResource = client
-					.resource("http://127.0.0.1:8080/api/people");
+//					.resource("http://127.0.0.1:8080/api/people");
+					.resource("http://127.0.0.1:8080/api/entitytest-2-s");
 
-			String input = "{\"name\":\"Test2aa\",\"surname\":\"Test22aa\"}";
+
+			String input = "{\"test1\":\"" + base64String + "\", \"test1ContentType\":\"image/jpeg\"}";
 
 			ClientResponse response = webResource.type("application/json")
 					.post(ClientResponse.class, input);
