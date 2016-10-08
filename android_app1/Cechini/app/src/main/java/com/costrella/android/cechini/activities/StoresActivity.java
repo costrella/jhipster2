f852c6788d019bf8390572eeb3c77666c666e3eb
Michal Kostrzewa <misiek.mk@gmail.com>
2016-10-08 19:24:50 +0200

    StoresActivity

--- android_app1/Cechini/app/src/main/java/com/costrella/android/cechini/activities/StoresActivity.java
+++ android_app1/Cechini/app/src/main/java/com/costrella/android/cechini/activities/StoresActivity.java
@@ -2,77 +2,52 @@ package com.costrella.android.cechini.activities;
 
 import android.app.ListActivity;
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.TextView;
 
 import com.costrella.android.cechini.R;
-import com.costrella.android.cechini.model.Day;
+import com.costrella.android.cechini.model.Store;
 
 import java.util.ArrayList;
 
-public class DaysActivity extends ListActivity {
+public class StoresActivity extends ListActivity {
 
     private TextView text;
-    private ArrayList<Day> listValues;
+    private ArrayList<Store> listValues;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_days);
-
+        setContentView(R.layout.activity_stores);
         listValues = new ArrayList<>();
-// Create the adapter to convert the array to views
-        DayAdapter adapter = new DayAdapter(this, listValues);
-
+        StoreAdapter adapter = new StoreAdapter(this, listValues);
 
         text = (TextView) findViewById(R.id.mainText);
 
-        Day d1 = new Day();
-        d1.setName("pon");
+        Store d1 = new Store();
+        d1.setName("store1");
 
-        Day d2 = new Day();
-        d2.setName("wt");
+        Store d2 = new Store();
+        d2.setName("store2");
 
         listValues.add(d1);
         listValues.add(d2);
-
-        // initiate the listadapter
-//        ArrayAdapter<Day> myAdapter = new ArrayAdapter<String>(this,
-//                R.layout.day_row_layout, R.id.listText, listValues);
-
-        // assign the list adapter
         setListAdapter(adapter);
-
     }
 
-    // when an item of the list is clicked
-    @Override
-    protected void onListItemClick(ListView list, View view, int position, long id) {
-        super.onListItemClick(list, view, position, id);
-
-        Day selectedItem = (Day) getListView().getItemAtPosition(position);
-
-        text.setText("You clicked " + selectedItem.getName() + " at position " + position);
-
-        Intent intent = new Intent(this, StoresActivity.class);
-        startActivity(intent);
-    }
-
-    public class DayAdapter extends ArrayAdapter<Day> {
-        public DayAdapter(Context context, ArrayList<Day> users) {
-            super(context, 0, users);
+    public class StoreAdapter extends ArrayAdapter<Store> {
+        public StoreAdapter(Context context, ArrayList<Store> stores) {
+            super(context, 0, stores);
         }
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
             // Get the data item for this position
-            Day day = getItem(position);
+            Store day = getItem(position);
             // Check if an existing view is being reused, otherwise inflate the view
             if (convertView == null) {
                 convertView = LayoutInflater.from(getContext()).inflate(R.layout.day_row_layout, parent, false);
@@ -87,5 +62,4 @@ public class DaysActivity extends ListActivity {
             return convertView;
         }
     }
-
 }
