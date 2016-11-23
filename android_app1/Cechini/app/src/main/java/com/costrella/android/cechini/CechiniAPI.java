52a782b00191ce7a7cb2ef74c4242b0c7f3e3d2b
Michal Kostrzewa <misiek.mk@gmail.com>
2016-11-23 22:28:46 +0100

    nazwa trasowki

--- android_app1/Cechini/app/src/main/java/com/costrella/android/cechini/CechiniAPI.java
+++ android_app1/Cechini/app/src/main/java/com/costrella/android/cechini/CechiniAPI.java
@@ -19,8 +19,8 @@ import retrofit2.http.Path;
  * Created by mike on 2016-09-15.
  */
 public interface CechiniAPI {
-//    String ENDPOINT = "http://195.149.224.137:8080/cechini/api/";
-    String ENDPOINT = "http://192.168.0.12:8080/api/";
+    String ENDPOINT = "http://195.149.224.137:8080/cechini/api/";
+//    String ENDPOINT = "http://192.168.0.12:8080/api/";
 
     @GET("stores/{storeId}")
     Call<Store> getStore(@Path("storeId") String storeId);
--- android_app1/Cechini/app/src/main/java/com/costrella/android/cechini/activities/CalendarActivity.java
+++ android_app1/Cechini/app/src/main/java/com/costrella/android/cechini/activities/CalendarActivity.java
@@ -7,6 +7,7 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.costrella.android.cechini.Constants;
@@ -38,53 +39,13 @@ public class CalendarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+        final EditText weekName = (EditText) this.findViewById(R.id.weekName);
         final MaterialCalendarView mcv = (MaterialCalendarView) findViewById(R.id.calendarView);
         mcv.state().edit()
                 .setMinimumDate(CalendarDay.from(2016, 9, 1))
                 .setCalendarDisplayMode(CalendarMode.MONTHS)
                 .commit();
         mcv.setSelectionMode(MaterialCalendarView.SELECTION_MODE_RANGE);
-//        mcv.addDecorator(new DayViewFacade().setDaysDisabled(true));
-
-        Call<List<Week>> callPersonWeeks = CechiniService.getInstance().getCechiniAPI().getPersonWeeks(PersonService.PERSON.getId());
-//wypelnienie
-//        callPersonWeeks.enqueue(new Callback<List<Week>>() {
-//            @Override
-//            public void onResponse(Call<List<Week>> call, Response<List<Week>> response) {
-//                final int code = response.code();
-//                if (code == 200) {
-//                    List<Week> weeks = response.body();
-//                    for(Week week : weeks){
-//                        Call<List<Day>> callWeekDays = CechiniService.getInstance().getCechiniAPI().getWeekDays(week.getId());
-//                        callWeekDays.enqueue(new Callback<List<Day>>() {
-//                            @Override
-//                            public void onResponse(Call<List<Day>> call, Response<List<Day>> response) {
-//                                int code = response.code();
-//                                if (code == 200) {
-//                                    List<Day> days = response.body();
-//                                    for(Day day: days){
-//                                        mcv.setDateSelected(day.getDate(), true);
-//                                    }
-//                                }
-//                            }
-//
-//                            @Override
-//                            public void onFailure(Call<List<Day>> call, Throwable t) {
-//
-//                            }
-//                        });
-//                    }
-//                    //FIXME czy na pewno tutaj?
-//                    Intent intent = new Intent(getApplicationContext(), TrasowkaActivity.class);
-//                    startActivity(intent);
-//                }
-//            }
-//
-//            @Override
-//            public void onFailure(Call<List<Week>> call, Throwable t) {
-//
-//            }
-//        });
 
         mcv.setOnDateChangedListener(new OnDateSelectedListener() {
             @Override
@@ -96,14 +57,18 @@ public class CalendarActivity extends AppCompatActivity {
             @Override
             public void onRangeSelected(@NonNull MaterialCalendarView widget, final @NonNull List<CalendarDay> dates) {
                 final Week week = new Week();
+                week.setName(weekName.getText().toString());
                 String from = "";
                 String to = "";
-                if(!dates.isEmpty()){
+                if (!dates.isEmpty()) {
                     from = new SimpleDateFormat("dd/MM/yyyy").format(dates.get(0).getDate());
-                    to = new SimpleDateFormat("dd/MM/yyyy").format(dates.get(dates.size()-1).getDate());
+                    to = new SimpleDateFormat("dd/MM/yyyy").format(dates.get(dates.size() - 1).getDate());
                 }
-
-                week.setName("Trasówka "+ from + " - " + to);
+                if (!weekName.getText().toString().isEmpty() && !weekName.getText().toString().equals("")) {
+                    week.setName(weekName.getText().toString());
+                }else{
+                    week.setName("Trasówka " + from + " - " + to);
+                }
 
                 Snackbar snackbar = Snackbar
                         .make(widget, "Czy utworzyć trasówkę?", Snackbar.LENGTH_LONG)
@@ -121,7 +86,7 @@ public class CalendarActivity extends AppCompatActivity {
                                     @Override
                                     public void onResponse(Call<Week> call, Response<Week> response) {
                                         int code = response.code();
-                                        Toast.makeText(getApplicationContext(), "creating week... " + code, Toast.LENGTH_LONG).show();
+//                                            Toast.makeText(getApplicationContext(), "creating week... " + code, Toast.LENGTH_LONG).show();
                                         if (code == 201) {
 
                                             final Week week = response.body();
@@ -131,9 +96,6 @@ public class CalendarActivity extends AppCompatActivity {
                                                 day.setDate(calendarDay.getDate());
                                                 day.setName(DateUtil.getDayName(calendarDay));
                                                 day.setWeek(week);
-//                                                Set<Store> stores = new HashSet<>();
-//                                                stores.addAll(StoreService.STORES_LIST);
-//                                                day.setStores(stores);
                                                 days.add(day);
 
                                             }
@@ -143,7 +105,7 @@ public class CalendarActivity extends AppCompatActivity {
                                                 public void onResponse(Call<List<Day>> call, Response<List<Day>> response) {
                                                     int code = response.code();
                                                     if (code == 201) {
-                                                        Toast.makeText(getApplicationContext(), "OK", Toast.LENGTH_LONG).show();
+//                                                            Toast.makeText(getApplicationContext(), "OK", Toast.LENGTH_LONG).show();
                                                         Call<List<Day>> callDaysWeek = CechiniService.getInstance().getCechiniAPI().getWeekDays(week.getId());
                                                         callDaysWeek.enqueue(new Callback<List<Day>>() {
                                                             @Override
@@ -155,9 +117,6 @@ public class CalendarActivity extends AppCompatActivity {
                                                                     DayService.DAYS.clear();
                                                                     DayService.DAYS.addAll(response.body());
                                                                     startActivity(intent);
-
-                                                                    //TESTOWO!!!!
-
                                                                 }
                                                             }
 
@@ -167,13 +126,13 @@ public class CalendarActivity extends AppCompatActivity {
                                                             }
                                                         });
                                                     } else {
-                                                        Toast.makeText(getApplicationContext(), "BAD1", Toast.LENGTH_LONG).show();
+                                                        Toast.makeText(getApplicationContext(), Constants.SOMETHING_WRONG, Toast.LENGTH_LONG).show();
                                                     }
                                                 }
 
                                                 @Override
                                                 public void onFailure(Call<List<Day>> call, Throwable t) {
-                                                    Toast.makeText(getApplicationContext(), "BAD2", Toast.LENGTH_LONG).show();
+                                                    Toast.makeText(getApplicationContext(), Constants.SOMETHING_WRONG, Toast.LENGTH_LONG).show();
                                                 }
                                             });
 
@@ -194,21 +153,6 @@ public class CalendarActivity extends AppCompatActivity {
                 snackbar.show();
 
 
-//                new AlertDialog.Builder(getApplicationContext())
-//                        .setIcon(android.R.drawable.ic_dialog_alert)
-//                        .setTitle("Potwierdź")
-//                        .setMessage("Czy dodać trasówkę na: " + dates.size() + " dni")
-//                        .setPositiveButton("Tak", new DialogInterface.OnClickListener() {
-//                            @Override
-//                            public void onClick(DialogInterface dialog, int which) {
-//                                finish();
-//                                );
-//
-//                            }
-//
-//                        })
-//                        .setNegativeButton("Nie", null)
-//                        .show();
             }
         });
 
